611f0d5e4b8d83728e38707f4f7b7b7c328a01e7
Alix Silvert <alix.silvert@protonmail.com>
2022-10-10 17:04:26 +0200

    added Integration step

--- pipeline/rules/normalisation.R
+++ pipeline/rules/normalisation.R
@@ -14,11 +14,20 @@ args = commandArgs(trailingOnly=TRUE)
 rds_file_path = args[1]
 samples_info_path = args[2]
 output = args[3]
-outputDirectory = args[4]
-cell_cycle_markers_path = args[5]
+cell_cycle_markers_path = args[4]
 
 seurat_data = readRDS(file = rds_file_path)
 cell_cycle_markers = readRDS(cell_cycle_markers_path)
+
+######################################
+### GETTING THE DATA TO REGRESS ON ###
+######################################
+
+
+GPhase <- cell_cycle_markers %>%
+  filter(phase == "G2/M")
+SPhase <- cell_cycle_markers %>%
+  filter(phase == "S")
 
 ###################
 ### NORMALISING ###
@@ -27,6 +36,17 @@ cell_cycle_markers = readRDS(cell_cycle_markers_path)
 # We are gonne use a normalization based on SCTransform while correcting for
 # phase cycle (as this is not of interest to us)
 
-split_seurat = SplitObject(seurat_data, split.by = "sample")
-#This crate a list of sub object, each will be normalised on their own.
-print(cell_cycle_markers)
+
+split_seurat = SplitObject(seurat_data, split.by = "HTO_classification")
+
+
+#We split the seurat object before normalisation because of possible
+#pre sequencing factors
+for (i in 1:length(split_seurat)) {
+  split_seurat[[i]] <- NormalizeData(split_seurat[[i]], verbose = TRUE)
+  split_seurat[[i]] <- CellCycleScoring(split_seurat[[i]], g2m.features=GPhase, s.features=SPhase)
+  split_seurat[[i]] <- SCTransform(split_seurat[[i]], vars.to.regress = c("percent.mt", "G2M.Score", "S.Score"))
+}
+
+
+saveRDS(split_seurat, file = output)
